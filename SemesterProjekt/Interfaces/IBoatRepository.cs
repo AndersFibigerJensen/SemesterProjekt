cb7b21ee2d4743ce41833826add414d197a975a1
Mammon22 <anders99fib@gmail.com>
2022-11-28 15:36:43 +0100

    Boat Class og Interface

---
+++ SemesterProjekt/Interfaces/IBoatRepository.cs
@@ -0,0 +1,21 @@
+﻿using SemesterProjekt.Model;
+
+namespace SemesterProjekt.Interfaces
+{
+    public interface IBoatRepository
+    {
+        List<Boat> GetAllBoats();
+
+        Boat GetBoat(int id);
+
+        void AddBoat();
+
+        void EditBoat();
+
+        void DeleteBoat();
+
+        
+
+
+    }
+}
---
+++ SemesterProjekt/Model/Boat.cs
@@ -0,0 +1,20 @@
+﻿namespace SemesterProjekt.Model
+{
+    public class Boat
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public double Length { get; set; }
+
+        public double Width { get; set; }
+
+        public double Weight { get; set; }
+
+        public double TopSpeed { get; set; }
+
+        public int MinimumCrew { get; set; }
+
+    }
+}
