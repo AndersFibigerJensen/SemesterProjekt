4e8d6793de9875e855205d14ea7ce24a617a8eb2
Mammon22 <anders99fib@gmail.com>
2022-11-28 19:47:54 +0100

    JsonBoat

---
+++ SemesterProjekt/Services/JsonIboatRepository.cs
@@ -0,0 +1,35 @@
+﻿using SemesterProjekt.Interfaces;
+using SemesterProjekt.Model;
+
+namespace SemesterProjekt.Services
+{
+    public class JsonIboatRepository : IBoatRepository
+    {
+        string filepath =@"Data\JsonBoats";
+
+        public void AddBoat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteBoat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void EditBoat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Boat> GetAllBoats()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Boat GetBoat(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
