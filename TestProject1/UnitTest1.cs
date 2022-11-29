8bad9e988f23864b4989cffab7a7f9460b35af66
Mammon22 <anders99fib@gmail.com>
2022-11-29 12:53:24 +0100

    Test

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,11 +1,19 @@
+using SemesterProjekt.Model;
+
 namespace TestProject1
 {
     [TestClass]
     public class UnitTest1
     {
+
+        //setup
+        Boat Andre= new Boat({ 1, "andre", 2.50, 1.00, 800, 14.05, 2 });
+
+
         [TestMethod]
         public void TestMethod1()
         {
+
         }
     }
 }
