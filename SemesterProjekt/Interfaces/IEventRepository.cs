0ee846423b776d44e54a067e2be9f906607e097d
Tor Sander Pleh Avnbo <torx0206@edu.zealand.dk>
2022-11-29 12:23:02 +0100

    added Event

---
+++ SemesterProjekt/Interfaces/IEventRepository.cs
@@ -0,0 +1,14 @@
+﻿using SemesterProjekt.Model;
+namespace SemesterProjekt
+{
+    public interface IEventRepository
+    {
+        List<Event> GetAllEvents();
+        Event GetEvent(int id);
+        void AddEvent(Event e);
+        void RemoveEvent(int id);
+        void EditEvent(Event e);
+
+        
+    }
+}
---
+++ SemesterProjekt/Model/Event.cs
@@ -0,0 +1,13 @@
+﻿namespace SemesterProjekt.Model
+{
+    public class Event
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        //public List<Event> joinedMembers { get; set; }
+        public List<Boat> joinedBoats { get; set; }
+
+    }
+}
