0f9b841fb5bdb08a34809ac953c4b5ec5f64e310
Mammon22 <anders99fib@gmail.com>
2022-12-05 10:57:25 +0100

    Edit

--- SemesterProjekt/Pages/Boatmodel/EditBoat.cshtml.cs
+++ SemesterProjekt/Pages/Boatmodel/EditBoat.cshtml.cs
@@ -1,12 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SemesterProjekt.Interfaces;
+using SemesterProjekt.Model;
 
 namespace SemesterProjekt.Pages.Boatmodel
 {
     public class EditBoatModel : PageModel
     {
-        public void OnGet()
+        private IBoatRepository boatRepository;
+
+        [BindProperty]
+        public Boat Boat { get; set; }
+
+        public EditBoatModel(IBoatRepository boatRepository)
         {
+            this.boatRepository = boatRepository;
+        }
+
+        public void OnGet(int id)
+        {
+            Boat = boatRepository.GetBoat(id);
+        }
+
+        public IActionResult Onpost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            boatRepository.AddBoat(Boat);
+            return RedirectToPage("IndexBoat");
         }
     }
 }
