2a621caef8283bef14a795bd2053a61b42ac8abd
Tor Sander Pleh Avnbo <torx0206@edu.zealand.dk>
2022-12-12 10:10:08 +0100

    finished Blog

--- SemesterProjekt/Pages/Blogmodel/EditBlogPost.cshtml.cs
+++ SemesterProjekt/Pages/Blogmodel/EditBlogPost.cshtml.cs
@@ -1,12 +1,37 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SemesterProjekt.Interfaces;
+using SemesterProjekt.Model;
 
 namespace SemesterProjekt.Pages.Blogmodel
 {
     public class EditBlogPostModel : PageModel
     {
-        public void OnGet()
+        private IBlogRepository blogRepository;
+
+        [BindProperty]
+        public BlogPost Post { get; set; }
+
+        public EditBlogPostModel(IBlogRepository blogRepository)
         {
+            this.blogRepository = blogRepository;
         }
+
+        public void OnGet(int postId)
+        {
+            Post = blogRepository.GetBlogPost(postId);
+        }
+
+        public IActionResult Onpost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            blogRepository.EditBlogPost(Post);
+            return RedirectToPage("IndexBlog");
+
+        }
+        
     }
 }
